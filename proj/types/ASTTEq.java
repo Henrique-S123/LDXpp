ea939076dc6e42d5c35d5374d2d51c94299c002d
Henrique Soares <henrique.c.soares@tecnico.ulisboa.pt>
2026-03-29 18:18:07 +0100

    added equality types.

---
+++ proj/types/ASTTEq.java
@@ -0,0 +1,37 @@
+package proj.types;
+
+import proj.ast.*;
+import proj.env.*;
+import proj.errors.*;
+
+public class ASTTEq implements ASTType {
+    ASTNode term1, term2;
+    ASTType type;
+
+    public ASTTEq(ASTNode t1, ASTNode t2, ASTType t) {
+        term1 = t1;
+        term2 = t2;
+        type = t;
+    }
+
+    public ASTNode getTerm1() {
+        return term1;
+    }
+
+    public ASTNode getTerm2() {
+        return term2;
+    }
+
+    public ASTType getType() {
+        return type;
+    }
+
+    public String toStr() {
+        return term1+"≡"+ term2+":"+type.toStr();
+    }
+
+    public boolean isSubtypeOf(ASTType o, EnvSet e) throws InterpreterError {
+        return false;
+    }
+}
+
