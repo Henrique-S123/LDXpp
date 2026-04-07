8fd0b4707db0122b43d5d7c93b839c7486b13fc1
Henrique Soares <henrique.c.soares@tecnico.ulisboa.pt>
2026-04-07 20:32:35 +0100

    implemented normalize and equals methods for tensor and ops.

--- proj/ast/ASTArithOp.java
+++ proj/ast/ASTArithOp.java
@@ -15,6 +15,18 @@ public class ASTArithOp implements ASTNode {
 		op = o;
     }
 
+	public ASTNode getLhs() {
+		return lhs;
+	}
+
+	public ASTNode getRhs() {
+		return rhs;
+	}
+
+	public String getOp() {
+		return op;
+	}
+
     public IValue eval(Environment<IValue> e) throws InterpreterError {
 		IValue v1 = lhs.eval(e);
 		IValue v2 = rhs.eval(e);
@@ -67,6 +79,15 @@ public class ASTArithOp implements ASTNode {
 	}
 
 	public ASTNode normalize() {
-        return this;
+		return new ASTArithOp(lhs.normalize(), rhs.normalize(), op);
     }
+
+	public boolean equals(Object o) {
+		return o instanceof ASTArithOp && ((ASTArithOp) o).getLhs().equals(lhs)
+			&& ((ASTArithOp) o).getRhs().equals(rhs) && ((ASTArithOp) o).getOp().equals(op);
+	}
+
+	public String toString() {
+		return String.format("%s %s %s", lhs.toString(), op, rhs.toString());
+	}
 }
--- proj/ast/ASTTensor.java
+++ proj/ast/ASTTensor.java
@@ -12,6 +12,14 @@ public class ASTTensor implements ASTNode {
         first = f;
         second = s;
     }
+
+    public ASTNode getFirst() {
+        return first;
+    }
+
+    public ASTNode getSecond() {
+        return second;
+    }
     
     public IValue eval(Environment<IValue> e) throws InterpreterError {
         IValue v1 = first.eval(e);
@@ -26,6 +34,17 @@ public class ASTTensor implements ASTNode {
     }
 
     public ASTNode normalize() {
-        return this;
+        return new ASTTensor(first.normalize(), second.normalize());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return o instanceof ASTTensor &&
+            first.equals(((ASTTensor) o).getFirst()) && second.equals(((ASTTensor) o).getSecond());
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%s ⊗ %s)", first.toString(), second.toString());
     }
 }
