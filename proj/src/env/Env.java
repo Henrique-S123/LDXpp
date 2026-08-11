773028320daaad20eae11494d8e3ba768666da69
Henrique Soares <henrique.c.soares@tecnico.ulisboa.pt>
2026-08-11 14:31:30 +0100

    add suggestion to eq related methods.

--- proj/src/env/Env.java
+++ proj/src/env/Env.java
@@ -73,6 +73,7 @@ public class Env<E>{
 
     public ASTNode findEq(String bid) {
         // heuristic: the term to solve is usually on the left side.
+        // try: add && !teq.isHyp() to if condition
         Env<E> curr = this;
         while (curr != null) {
             for (Binder<E> b : curr.bindings.values())
@@ -97,7 +98,7 @@ public class Env<E>{
             for (Map.Entry<String, Binder<E>> entry : curr.bindings.entrySet())
                 if (entry.getValue().val instanceof ASTTEq teq) {
                     // TODO: try to optimize by not using trivial equalities
-                    // if (teq.getTerm1() instanceof ASTId || teq.getTerm2() instanceof ASTId) continue;
+                    // e.g.: add && teq.isHyp() to if condition
                     Debug.log("Testing proof: " + entry.getValue());
                     Debug.open();
                     E res = null;
